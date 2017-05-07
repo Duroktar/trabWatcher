bb9e64342f3e9328277848350e23c7574a4b44c3
Duroktar <duroktar@gmail.com>
2017-05-06 23:03:20 -0300

    refactor main.go into watcher.go

--- watcher.go
+++ watcher.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/logrusorgru/aurora"
@@ -104,4 +106,23 @@ func (w Watcher) killProcess() {
 			panic(err)
 		}
 	}
+}
+
+func handleCtrlC(c chan os.Signal) {
+	sig := <-c
+	fmt.Println(aurora.Green("\rSignal: "), sig)
+	if sig == os.Interrupt {
+		fmt.Println(aurora.Magenta("\rGoodbye"))
+		os.Exit(0)
+	}
+}
+
+func main() {
+	fmt.Println(aurora.Magenta("Starting watcher."))
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	go handleCtrlC(c)
+
+	NewWatcher(os.Args[1], os.Args[2:]).Start()
 }
